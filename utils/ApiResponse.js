b0251b5c2593cbeba6be643fa83f46c4dc624c2e
ashrith-devison <ashrithsai.devison@gmail.com>
2024-07-09 22:47:16 +0530

    Api Response Util setup done

---
+++ utils/ApiResponse.js
@@ -0,0 +1,11 @@
+class ApiResponse {
+    constructor(status, message, data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    static send(res, status, message, data) {
+        return res.status(status).send(new ApiResponse(status, message, data));
+    }
+}
